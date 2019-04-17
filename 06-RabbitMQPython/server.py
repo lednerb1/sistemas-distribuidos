cd0db9f694da39c51ea7a410537a5a4027b79ea0
Peter Brendel <peter_brendel@hotmail.com>
2019-04-17 21:40:06 +0000

    Working

--- 06-RabbitMQPython/server.py
+++ 06-RabbitMQPython/server.py
@@ -6,14 +6,16 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(
 channel = connection.channel()
 
 channel.exchange_declare(exchange='plogs', exchange_type='fanout')
+channel.exchange_declare(exchange='ulogs', exchange_type='fanout')
 
-result = channel.queue_declare(' ',exclusive=True)
+result = channel.queue_declare('',exclusive=True)
 
 queueName = result.method.queue
 
 channel.queue_bind(exchange='plogs', queue=queueName)
 
 def newMessageCallback(ch, method, properties, msg):
+    """
     print('--')
     print(ch)
     print('--')
@@ -23,7 +25,13 @@ def newMessageCallback(ch, method, properties, msg):
     print('--')
     print(" [x] Received {}".format(msg))
     print('--')
-    channel.basic_publish(exchange='plogs', routing_key='', body=msg.upper())
+    """
+    msg = msg.decode()
+    nome, id, data = msg.split('\\>>\\')
+    print(nome, id, data)
+    file = open(nome+'-'+id+'.serv', 'w')
+    file.write(data)
+    channel.basic_publish(exchange='ulogs', routing_key='', body=msg)
 
 channel.basic_consume(newMessageCallback, queue=queueName, no_ack=True)
 
